363866c4fcf3deef8395e8ae8895189f14883f2d
Gector <93502005+GectorCh@users.noreply.github.com>
2023-11-02 10:26:17 +0700

    :>

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,5 @@
 <?php
-$conn=mysqli_connect('localhost','root','','gcpm3199_gcpetro');
+$conn=mysqli_connect('localhost','gcpm3199','SujtZNAPtDq448','gcpm3199_gcpetro');
 /* check connection */
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
