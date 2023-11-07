07661a77aa78dcc0b33c92bc6165f4cf8a209e33
Gector <93502005+GectorCh@users.noreply.github.com>
2023-11-07 08:20:34 +0700

    :>

--- tampil_user.php
+++ tampil_user.php
@@ -1,11 +1,6 @@
 <?php 
     include "header.php";
 ?>
-<?php
-    if($_SESSION['role'] == 'admin'){
-       
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -16,10 +11,10 @@
 </head>
 <style>
 body {
-  background-image: url('12.gif.gif');
+  background-image: url('backhome.jpg');
   background-repeat: no-repeat;
   background-attachment: fixed;
-  background-size: 100% 100%;
+  background-size: cover;
 }
 </style>
 <body>
@@ -56,32 +51,8 @@ body {
             ?>
         </tbody>
     </table>
+    <a style="margin-right:10px;" href="tambah_user.php" class="btn btn-primary">Tambah User</a>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 </body>
 </html>
-<?php
-    }
-
-?>
-<?php
-    if($_SESSION['role'] == 'kasir'){
-       
-?>
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-    <title></title>
-</head>
-<body>
-   
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
-</body>
-</html>
-<?php
-    }
-
-?>
