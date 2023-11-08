523dabd202abd6758615cd2d2a6c36bf6cea12b4
Gector <93502005+GectorCh@users.noreply.github.com>
2023-11-08 08:10:57 +0700

    :>

--- hapus_paket.php
+++ hapus_paket.php
@@ -1,7 +1,7 @@
 <?php 
-        if($_GET['id']){
+        if($_GET['id_merek']){
             include "koneksi.php";
-            $qry_hapus=mysqli_query($conn,"delete from paket where id_paket='".$_GET['id']."'");
+            $qry_hapus=mysqli_query($conn,"delete from merek where id_merek='".$_GET['id_merek']."'");
             if($qry_hapus){
                 echo "<script>alert('Sukses hapus paket');location.href='tampil_paket.php';</script>";
             } else {
