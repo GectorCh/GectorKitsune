5cb24c2215a6efd70259745749d35adab74d4de0
AM-kitsune <105692663+AM-kitsune@users.noreply.github.com>
2023-11-08 09:09:00 +0700

    Update home.php

--- home.php
+++ home.php
@@ -1,11 +1,11 @@
 <?php 
     include "header.php";
 ?>
-<h2 style="background-color:rgb(255, 255, 255);">Selamat Datang <?=$_SESSION['nama']?> di GCPetro</h2>
+<h2 style="color: lightgray;">Selamat Datang <?=$_SESSION['nama']?> di GCPetro</h2>
 <h5>Anda Login Sebagai : <?=$_SESSION['role']?> </h5>
 <style>
 body {
-  background-image: url('backhome.jpg');
+  background-image: linear-gradient(rgba(0,80,0,0.65), rgba(0,80,0,0.65)), url('backhome.jpg');
   background-repeat: no-repeat;
   background-attachment: fixed;
   background-size: cover;
