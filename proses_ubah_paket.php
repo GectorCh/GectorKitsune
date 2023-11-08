1bf01e46bb6ddb596842497aefaaa12c359bbaff
AM-kitsune <105692663+AM-kitsune@users.noreply.github.com>
2023-11-08 08:27:45 +0700

    Update proses_ubah_paket.php

--- proses_ubah_paket.php
+++ proses_ubah_paket.php
@@ -1,17 +1,17 @@
 <?php
     if($_POST){
-        $id=$_POST['id_paket'];
-        $jenis=$_POST['jenis'];
+        $id=$_POST['id_merek'];
+        $jenis_merek=$_POST['jenis_merek'];
         $harga=$_POST['harga'];
-        if(empty($jenis)){
-            echo "<script>alert('Jenis Paket tidak boleh kosong');location.href='ubah_paket.php';</script>";
+        if(empty($jenis_merek)){
+            echo "<script>alert('Jenis merek tidak boleh kosong');location.href='ubah_paket.php';</script>";
         } else {
             include "koneksi.php";
-            $update=mysqli_query($conn,"update paket set jenis='".$jenis."', harga='".$harga."' where id_paket = '".$id."'") or die(mysqli_error($conn));
+            $update=mysqli_query($conn,"update merek set jenis_merek='".$jenis_merek."', harga='".$harga."' where id_merek = '".$id."'") or die(mysqli_error($conn));
             if($update){
                 echo "<script>alert('Sukses update paket');location.href='tampil_paket.php';</script>";
             } else {
-                echo "<script>alert('Gagal update paket');location.href='ubah_paket.php?id=".$id."';</script>";
+                echo "<script>alert('Gagal update paket');location.href='ubah_paket.php?id_merek=".$id."';</script>";
             }
         } 
     }
