c4c9b4d6d3b9cae630680f41fdd0eb41432a84aa
Gector <93502005+GectorCh@users.noreply.github.com>
2023-11-08 07:13:15 +0700

    :>

--- proses_tambah_paket.php
+++ proses_tambah_paket.php
@@ -1,12 +1,12 @@
 <?php
     if($_POST){
-        $jenis=$_POST['jenis'];
-        $harga=$_POST['harga'];
-        if(empty($jenis)){
+        $jenis_merek=$_POST['jenis_merek'];
+        $harga_merek=$_POST['harga_merek'];
+        if(empty($jenis_merek)){
             echo "<script>alert('Jenis tidak boleh kosong');location.href='tambah_paket.php';</script>";
         } else {
             include "koneksi.php";
-            $insert=mysqli_query($conn,"insert into paket (jenis,harga) value ('".$jenis."','".$harga."')") or die(mysqli_error($conn));
+            $insert=mysqli_query($conn,"insert into merek (jenis_merek,harga_merek) value ('".$jenis_merek."','".$harga_merek."')") or die(mysqli_error($conn));
             if($insert){
                 echo "<script>alert('Sukses menambahkan paket');location.href='tampil_paket.php';</script>";
             } else {
