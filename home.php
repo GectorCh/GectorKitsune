deb98fafb42b1d266547ffb3cd586b502ce28b22
Jim Jam <105687147+JimJam2@users.noreply.github.com>
2023-11-13 08:30:42 +0700

    :>

--- home.php
+++ home.php
@@ -1,14 +1,30 @@
 <?php 
     include "header.php";
 ?>
+<div class= "box">
 <h2 style="color: lightgray;">Selamat Datang <?=$_SESSION['nama']?> di GCPetro</h2>
 <h5>Anda Login Sebagai : <?=$_SESSION['role']?> </h5>
+</div>
 <style>
 body {
   background-image: linear-gradient(rgba(0,80,0,0.65), rgba(0,80,0,0.65)), url('backhome.jpg');
   background-repeat: no-repeat;
   background-attachment: fixed;
   background-size: cover;
+}
+.box{
+  width: 450px;
+  padding: 30px;
+  position: absolute;
+  top: 20%;
+  left: 20%;
+  transform: translate(-50%,-50%);
+  background: #222222;
+  text-align: center;
+}
+.box h2, h5{
+  font-weight: 500;
+  font-family: "Montserrat", sans-serif;
 }
 </style>
 
