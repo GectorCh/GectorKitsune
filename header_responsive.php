71e135bb935c980db4e6932884585331e465e9de
Gector <93502005+GectorCh@users.noreply.github.com>
2023-11-15 14:32:52 +0700

    Removing any annoying codes

--- header_responsive.php
+++ header_responsive.php
@@ -12,7 +12,7 @@ session_start();
 <lnk rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;500&family=Poppins:wght@100;300;500&family=Roboto:wght@100;300;500&display=swap" rel="stylesheet">
 <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <title></title>
 </head>
